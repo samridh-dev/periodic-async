a4e59d78cad569c1d2e154e0482282ca180062a8
samridh-dev <singhsam@grinnell.edu>
2024-04-22 16:34:24 +0200

    implemented static lib option

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,4 +1,5 @@
 option(ENABLE_BUILD_LIBRARY "build main library"                          ON )
+option(ENABLE_BUILD_STATIC  "build static lib instead of shared"          OFF )
 option(ENABLE_BUILD_EXAMPLE "build the example binary"                    ON )
 option(ENABLE_BINARY_FOLDER "Dump all binaries to bin/"                   ON )
 option(ENABLE_DEBUG         "Set build type to debug"                     OFF )
