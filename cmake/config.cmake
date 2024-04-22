c457cc00a73bcbb75b56aea3e13cdeca6314a8da
samridh-dev <singhsam@grinnell.edu>
2024-04-22 14:11:30 +0200

    created library option

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,3 +1,4 @@
-option(ENABLE_BUILD_EXAMPLE "build the example binary"                    ON )
-option(ENABLE_BINARY_FOLDER "Dump all binaries to bin/"                   ON )
+option(ENABLE_BUILD_LIBRARY "build main library"                          ON )
+option(ENABLE_BUILD_EXAMPLE "build the example binary"                    OFF )
+option(ENABLE_BINARY_FOLDER "Dump all binaries to bin/"                   OFF )
 option(ENABLE_DEBUG         "Set build type to debug"                     OFF )
